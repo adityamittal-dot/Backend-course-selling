0e908e08f22e5eec21c2947760eca4cec9c7cd11
adityamittal-dot <adityamittal529@gmail.com>
2026-04-28 15:36:27 +0530

    Add basic routing structure

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 const express = require("express");
-const jwt = reqwuire("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+
+const app = express()
+
+app.post("/user/signup", function(req, res){
+  res.json({
+    message: "signup endpoint"
+  })
+})
+
+app.post("/user/signin", function(req, res){
+  res.json({
+    message: "signin endpoint"
+  })
+})
+
+app.get("/user/purchases", function(req, res){
+  res.json({
+    message: "purchases endpoint"
+  })
+})
+
+app.get("/courses", function(req, res){
+  res.json({
+    message: "courses endpoint"
+  })
+})
+
+app.post("/course/purchase", function(req, res){
+  res.json({
+    message: "purchase endpoint"
+  })
+})
+
+
+
+
+
+
+app.listen(3000);
